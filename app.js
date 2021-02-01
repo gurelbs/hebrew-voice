7c158a014a6f9b490969947c360b6f650a6759e8
gurelbs <guri240@gmail.com>
2021-02-01 23:12:59 +0200

    update

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 let askMore = `שאל אותי עוד משהו!`
 
-// let txt = ()=> {
-//   let val = document.getElementById('textareaValue').value
-//   return val
-// }
-
 let time = `השעה עכשיו ${new Date().getHours()} ו${new Date().getMinutes()} דקות.     `;
-let creator = `קוראים לך גוראל. אתה היוצר שלי!`
-let tryAgain = `לא הבנתי מה אתה אומר. תנסה שוב בבקשה.`
+let creator = `,קוראים לך גוּראֶל, אתה היוצר שלי, עשיתה אותי בשפת התוכנה ג'אוה Script`
+let tryAgain = `תנסה שוב בבקשה.`
 let moon = `המרחק לירח הוא 63 מיליון קילומטר`
-let yourName = `השם שלי הוא רמי. אני עוזר דיגיטלי בעברית.`
+let yourName = `השם שלי, הוא יוחאי. אני עוזר דיגיטלי בעברית.`
+
 let googleSearch = (e) => window.open(`https://www.google.com/search?q=${e}`) 
-let youtube = (e) => window.open(`https://www.youtube.com/results?search_query=${e}`) 
+let youtube = (e) => window.open(`https://www.youtube.com/results?search_query=${e}`)
+let arr = [];
+
+let wordSearch = someWord => {
+  someWord = someWord.split(' ')
+  arr.push(someWord)    
+  someWord = arr[0].slice(2).join(' ')
+   return someWord
+}
 let content = document.querySelector('.content');
 let btn = document.querySelector('.btn');
 
 let speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 let recognition = new speechRecognition();
-
-recognition.onstart = () => console.log('active');
+function reloadPage(){
+  document.location.reload();
+}
+recognition.onstart = () => console.log('זיהוי קולי פעיל');
 
 recognition.onresult = e => {
   const current = e.resultIndex;
@@ -31,8 +37,9 @@ recognition.onresult = e => {
            transcript == 'איך קוראים לי' ? creator : 
            transcript == 'מה המרחק לירח' ? moon : 
            transcript == 'מה השם שלך' ? yourName : 
-          googleSearch(transcript) && transcript == 'none',
-           
+           transcript.includes('חפש בגוגל') !== false ? googleSearch(wordSearch(transcript)) && location.reload():
+           transcript.includes('חפש ביוטיוב') !== false ? youtube(wordSearch(transcript)) && location.reload()
+           : `לא הבנתי מה אתה רוצה ממני, כשאתה אומר ${transcript}. ,` + tryAgain,
       hl: 'he-il',
       r:0, 
       c: 'mp3',
@@ -40,8 +47,9 @@ recognition.onresult = e => {
       ssml: false
     })
   }
+  content.textContent = transcript
+  console.log(transcript);
   speak()
-  return content.textContent = transcript
   
 }
 
